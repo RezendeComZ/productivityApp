6a9b190d0aba48347cf2f9ba71e9035802bd6c51
Gabriel Rezende <g@brielrezen.de>
2021-11-15 20:39:13 -0300

    Quantidades de colunas se adaptando ao tamanho da tela, resta dividir o conteúdo

--- front/src/App.js
+++ front/src/App.js
@@ -1,155 +1,58 @@
 import { useState, useEffect } from 'react';
 import Notas from './components/Notas';
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// desinstalar o 'react-bootstrap'
 
 function App() {
-  // const [notas, setNotas] = useState([])
   const [ todasAsNotas, settodasAsNotas] = useState(
     [
       {
           "_id": "618311c652916b5104f9e7a3",
           "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie mauris quis ex mattis, quis tempor eros vulputate. Cras cursus dignissim aliquet. Sed pulvinar diam vitae volutpat congue. Aliquam ornare nunc eu erat sodales, ut gravida augue egestas.",
-          "lists": [
-              [
-                  "Limpeza",
-                  "/cDetergente",
-                  "Amaciante"
-              ],
-              [
-                  "Comida",
-                  "Arroz",
-                  "/cFeijão"
-              ],
-              [
-                  "",
-                  "Pilha",
-                  "Cabo USB"
-              ]
-          ],
-          "labels": [
-              "importante",
-              "trabalho"
-          ],
-          "pinned": false,
-          "color": "blue",
-          "reminders": [
-              1635884911075
-          ],
-          "permissions": [
-              {
-                  "user": "joao1979",
-                  "edit": true
-              }
-          ],
-          "url": "listaCompras",
-          "__v": 0
       },
       {
           "_id": "618311cf52916b5104f9e7a5",
           "title": "Lista de compras",
           "body": "Comprar no mercado mais próximo:",
-          "lists": [
-              [
-                  "Limpeza",
-                  "/cDetergente",
-                  "Amaciante"
-              ],
-              [
-                  "Comida",
-                  "Arroz",
-                  "/cFeijão"
-              ],
-              [
-                  "",
-                  "Pilha",
-                  "Cabo USB"
-              ]
-          ],
-          "labels": [
-              "importante",
-              "trabalho"
-          ],
-          "pinned": false,
-          "color": "blue",
-          "reminders": [
-              1635884911075
-          ],
-          "permissions": [
-              {
-                  "user": "joao1979",
-                  "edit": true
-              }
-          ],
-          "url": "listaCompras",
-          "__v": 0
       },
       {
-          "_id": "618311d352916b5104f9e7a7",
-          "title": "Lista de compras",
-          "body": "terceira",
-          "lists": [
-              [
-                  "Limpeza",
-                  "/cDetergente",
-                  "Amaciante"
-              ],
-              [
-                  "Comida",
-                  "Arroz",
-                  "/cFeijão"
-              ],
-              [
-                  "",
-                  "Pilha",
-                  "Cabo USB"
-              ]
-          ],
-          "labels": [
-              "importante",
-              "trabalho"
-          ],
-          "pinned": false,
-          "color": "blue",
-          "reminders": [
-              1635884911075
-          ],
-          "permissions": [
-              {
-                  "user": "joao1979",
-                  "edit": true
-              }
-          ],
-          "url": "listaCompras",
-          "__v": 0
+          "_id": "618311cf52916b5104f9e7a51",
+          "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie mauris quis ex mattis, quis tempor eros vulputate. Cras cursus dignissim aliquet. Sed pulvinar diam vitae volutpat congue. Aliquam ornare nunc eu erat sodales, ut gravida augue egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie mauris quis ex mattis, quis tempor eros vulputate. Cras cursus dignissim aliquet. Sed pulvinar diam vitae volutpat congue. Aliquam ornare nunc eu erat sodales, ut gravida augue egestas.",
+      },
+      {
+          "_id": "618311cf52916b5104f9e7a5n",
+          "body": "Coisa única",
       }
     ]
   )
-  
+
+  let [ columnsDivsPre, setcolumnsDivsPre ] = useState([])
+  const notesByWidth = (noteSize = 250) => Math.floor((window.innerWidth - 20) / noteSize)
+  const columnsGenerator = () => {
+    columnsDivsPre = []
+    for (let i = 1; i <= notesByWidth(); i++) {
+      columnsDivsPre.push(i)
+    }
+    setcolumnsDivsPre(columnsDivsPre)
+  }
+
+  useEffect(() => { // Executa somente uma vez, para criar as colunas na inicialização. Evita loop.
+    columnsGenerator()
+  }, [])
+
+    window.addEventListener('resize', () => {
+      if (columnsDivsPre.length !== notesByWidth()) {
+        columnsGenerator()
+      }
+    })
+
   return (
     <div id="desk">
-      <div className="column">
+    {columnsDivsPre.map(list => {
+      return <div className="column">
         <Notas notas={todasAsNotas}/>
       </div>
-    {/* <Header msg='Uma msg!'/>
-    <h1>Hello</h1> */}
-    {/* <Card style={{ color: "blue"}}>
-      <Card.Img />
-      <Card.Body>
-        <Card.Title>
-          Exemplo
-        </Card.Title>
-        <Card.Text>
-          Isso é um exemplo
-        </Card.Text>
-
-      </Card.Body>
-    </Card>
-    <Alert variant="primary">Isso é um alerta</Alert>
-    <Button>Botão teste</Button> */}
-
+    })}
     </div>
-  );
+  )
 }
 
 export default App;
--- front/src/components/Notas.js
+++ front/src/components/Notas.js
@@ -1,28 +1,15 @@
-// Preciso de uma função que crie as colunas de acordo com o tamanho
-
-let columns = [
-  {
-    n: 1, // necessário idenficar a coluna?
-    char: 2  
-  },
-  {
-    n: 2, // necessário idenficar a coluna?
-    char: 2  
-  }
-]
-
 const Notas = ({ titulo, color, notas}) => {
   return (
     <>
       {notas.map((nota) => {
         if (!nota.title) {
-        console.log("sem titulo")
+        {/* console.log("sem titulo") */}
         return (
         <div key={nota._id} className="nota">
         <p>{nota.body}</p>
         </div>)
         } else {
-        console.log("com titulo")
+        {/* console.log("com titulo") */}
         return (
         <div key={nota._id} className="nota">
         <h6>{nota.title}</h6>
