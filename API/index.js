fbe187c074f7f45fdd622c58fd973ebf41bfab8f
Gabriel Rezende <g@brielrezen.de>
2021-11-16 21:53:06 -0300

    Cors na API

--- API/index.js
+++ API/index.js
@@ -3,12 +3,14 @@ const PORT = process.env.PORT || 3001
 const MONGODB = process.env.MONGODB
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 
-mongoose.connect(MONGODB) // todo, colocar na .env
+mongoose.connect(MONGODB)
 mongoose.Promise = global.Promise // ver se isso ainda é necessário
 
 // Middlewares:
+app.use(cors())
 app.use(express.json())
 app.use('/api', require('./routes/api')) // importando aqui ao invés de usar: const routes = require('./routes/api')
 app.use(express.static('public'))
