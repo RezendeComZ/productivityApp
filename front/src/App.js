819a4cb0dd8abcad27ae3d0bf394402cf98500c6
Gabriel Rezende <g@brielrezen.de>
2021-11-19 22:45:49 -0300

    refatoração, porém ainda ruim

--- front/src/App.js
+++ front/src/App.js
@@ -18,38 +18,38 @@ function App() {
           "body": "4a, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie mauris quis ex mattis, quis tempor eros vulputate. Cras cursus dignissim aliquet. Sed pulvinar diam vitae volutpat congue. Aliquam ornare nunc eu erat sodales, ut gravida augue egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie mauris quis ex mattis, quis tempor eros vulputate. Cras cursus dignissim aliquet. Sed pulvinar diam vitae volutpat congue. Aliquam ornare nunc eu erat sodales, ut gravida augue egestas.",
       },
       {
-          "_id": "618311cf52916b5104f9e7a5n",
+          "_id": "618311cf52916b5104f9e7a5a",
           "body": "Coisa única, 5a",
       },
       {
-          "_id": "618311cf52916b5104f9e7a5n",
+          "_id": "618311cf52916b5104f9e7a59",
+          "body": "Lalalala, 6a",
+      },
+      {
+          "_id": "618311cf52916b5104f9e7a5m",
           "body": "Comportamento estranho, principalmente no resize. Última nota",
       }
     ]
   )
 
   let [ columnsDivsPre, setcolumnsDivsPre ] = useState([])
-  const notesByWidth = (noteSize = 250) => Math.floor((window.innerWidth - 20) / noteSize)
-
-  // let columnsContent = []
+  const processNotesByWidth = (noteSize = 250) => Math.floor((window.innerWidth - 20) / noteSize)
+  const notesByWidth = processNotesByWidth()
+  const colunaDaVez = () => Math.max.apply(null, (columnsDivsPre.map(coluna => parseInt(coluna.length))))
 
   const notasDistribuicao = () => {
     todasAsNotas.forEach(nota => {
-      let colunaDaVez = Math.max.apply(null, (columnsDivsPre.map(coluna => parseInt(coluna.length)))) // acho que tá sem lógica
-      columnsDivsPre[colunaDaVez].push(nota)
+      columnsDivsPre[colunaDaVez()].push(nota)
+      setcolumnsDivsPre(columnsDivsPre)
     })
-    setcolumnsDivsPre(columnsDivsPre)
-
   }
 
   const columnsGenerator = () => {
     columnsDivsPre = []
-    for (let i = 1; i <= notesByWidth(); i++) {
+    for (let i = 1; i <= notesByWidth; i++) {
       columnsDivsPre.push([])
     }
     notasDistribuicao()
-
-    console.log(columnsDivsPre);
   }
 
   useEffect(() => { // Executa somente uma vez, para criar as colunas na inicialização. Evita loop.
@@ -57,15 +57,15 @@ function App() {
   }, [])
 
   window.addEventListener('resize', () => {
-    if (columnsDivsPre.length !== notesByWidth()) {
+    if (columnsDivsPre.length !== notesByWidth) {
       columnsGenerator()
     }
   })
 
   return (
     <div id="desk">
-    {columnsDivsPre.map(list => {
-      return <div className="column">
+    {columnsDivsPre.map((list, index) => {
+      return <div className="column" key={index}>
         <Notas notas={list}/>
       </div>
     })}
